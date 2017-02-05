c6952ff8553f3a59a7928d392f45fe09427ac50a
Elena <elochka150899@gmail.com>
2017-02-05 18:03:08 +0200

    laba_1_commit

--- laba 1/laba 1 _11.py
+++ laba 1/laba 1 _11.py
@@ -1,17 +1,26 @@
-# Gorokhova Olena Knit 16-A
-days = range(1, 32)
-mounths = range(1,13)
-years = range(1901, 2016)
-d, m, y = int(input('day: ')),\
-int(input('mounth: ')),\
-int(input('year: '))
-if d in days and m in mounths and y in years:
-    if d == 31 and m == 12:
-        d = 1
-        m = 1
-        y += 1
+# Gorokhova Olena Knit  16-A
+while True:
+    days = range(1, 32)
+    mounths = range(1,13)
+    years = range(1901, 2016)
+    d, m, y = int(input('day: ')),\
+    int(input('mounth: ')),\
+    int(input('year: '))
+    if d in days and m in mounths and y in years:
+        if d == 31 and m == 12:
+            d = 1
+            m = 1
+            y += 1
+        else:
+            d+=1
+        print('next date:', d , '.', m , '.', y)
     else:
-        d+=1
-    print('next date:', d , '.', m , '.', y)
-else:
-    print("error")
+        print("error")
+    while True:
+        K = input("хотите продолжить? 1 - да, 2 - нет :")
+        if K == "1":
+            break
+        else:
+            flag = False
+            print("пока")
+            print()

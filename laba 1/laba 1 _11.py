6ed923e1ee96d74aa9fc9ba3965dfaf7e51b94c0
Elena Gorokhova <elochka150899@gmail.com>
2017-02-15 16:33:41 +0200

    Update laba 1 _11.py

--- laba 1/laba 1 _11.py
+++ laba 1/laba 1 _11.py
@@ -5,23 +5,27 @@ while N:
     days = range(1, 32)
     mounths = range(1,13)
     years = range(1901, 2016)
-    d, m, y = int(input('day: ')),\
-    int(input('mounth: ')),\
-    int(input('year: '))
-    if d in days and m in mounths and y in years:
-        if d==31:
-            if m==12:
-                y+=1
-                d = 1
-                m = 1
+    try:
+        d, m, y = int(input('day: ')),\
+        int(input('mounth: ')),\
+        int(input('year: '))
+
+        if d in days and m in mounths and y in years:
+            if d==31:
+                if m==12:
+                    y+=1
+                    d = 1
+                    m = 1
+                else:
+                    d = 1
+                    m+=1
             else:
-                d = 1
-                m+=1
+                d+=1
+            print('next date:', d , '.', m , '.', y)
         else:
-            d+=1
-        print('next date:', d , '.', m , '.', y)
-    else:
-        print("введены не корректные данные")
+            print("введены не корректные данные")
+    except ValueError:
+        print("введите число")
     while True:
         K = input("хотите продолжить? 1 - да, 2 - нет :")
         if K == "1":
@@ -29,3 +33,4 @@ while N:
         else:
             flag = False
             print("пока\n")
+

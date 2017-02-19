26e79581468a13138bc067225885727facf4ff4d
Elena <elochka150899@gmail.com>
2017-02-19 19:40:59 +0200

    laba_2_commit

--- laba 2/laba 2_var10_a.py
+++ laba 2/laba 2_var10_a.py
@@ -1,19 +1,26 @@
 k=True
 while k:
     v=set()
-    while len(v)<30:
+    while len(v)<7:
         try:
+            print("вам нужно ввести 30 чисел")
             v.add(int(input()))
             pass
         except ValueError:
             print("последовательность должна состоять из чисел")
     print("ваша последовательность :",v)
-    u=v
-    m=min(u)
-    M=max(u)
+    #u=v
+    m=min(v)
+    M=max(v)
     print ("минимальное число=",m,"максимальное число=",M)
     for i in range(m,M):
         if i not in v:
-            print(i)
+            print(i,end=",")
         else:
             continue
+    print()
+    a = input('Хотите продолжить? [y/n]: ')
+    if a == 'y':
+        continue
+    else:
+        break

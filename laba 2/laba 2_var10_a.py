54294483e96d17ea2415a3f9d3ed9856e173fbcc
Elena <elochka150899@gmail.com>
2017-02-19 18:50:26 +0200

    laba_2_commit

---
+++ laba 2/laba 2_var10_a.py
@@ -0,0 +1,19 @@
+k=True
+while k:
+    v=set()
+    while len(v)<7:
+        try:
+            v.add(int(input()))
+            pass
+        except ValueError:
+            print("последовательность должна состоять из чисел")
+    print("ваша последовательность :",v)
+    u=v
+    m=min(u)
+    M=max(u)
+    print ("минимальное число=",m,"максимальное число=",M)
+    for i in range(m,M):
+        if i not in v:
+            print(i)
+        else:
+            continue

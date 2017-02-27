21600e1a9f6c3457a9a72c353cfc127231fc0c5b
Elena <elochka150899@gmail.com>
2017-02-27 12:39:13 +0200

    laba_2_commit

--- laba 2/А 10 вар.py
+++ laba 2/А 10 вар.py
@@ -1,17 +1,18 @@
 k=True
 while k:
     v=set()
-    print("введите 7 чисел :")
-    while len(v)<7:
+    n = 30
+    print('введите', m, 'чисел :')
+    while len(v) < n:
         try:
             v.add(int(input()))
             pass
         except ValueError:
             print("последовательность должна состоять из целых чисел")
-    print("ваша последовательность :",v)
-    m=min(v)
-    M=max(v)
-    print ("минимальное число=",m,"максимальное число=",M)
+    print("ваша последовательность :", v)
+    m = min(v)
+    M = max(v)
+    print("минимальное число=", m, "максимальное число=", M)
     B = {i for i in range(m, M + 1)}
     print("числа которые не входят в последовательность:", B-v)
     a = input('Хотите продолжить? [y/n]: ')

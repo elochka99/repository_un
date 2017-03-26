a14cba2481a87bf775978074b4c57ac58e52cf96
Elena <elochka150899@gmail.com>
2017-03-26 21:04:01 +0300

    laba_3_commit

---
+++ laba 3/laba 3_3.3.1 (lin).py
@@ -0,0 +1,20 @@
+import timeit
+setup = '''
+import numpy as np
+#import random as rnd
+n = int(input('Количество элементов в массиве: '))
+a = np.zeros(n, dtype = np.int_)
+for j in range(n):
+    a[j] = int(input('Вводите элменти массива: '))
+x = float(input('искомый элемент x:  '))
+i = 0
+'''
+stmt = '''
+while i < n and a[i] != x:
+    i += 1
+if i == n:
+    print('элемент не найден')
+else:
+    print('элемент', x, 'находится в позиции', i)
+'''
+print('время поиска', timeit.timeit(stmt = stmt, setup = setup, number = 1), 'секунд')

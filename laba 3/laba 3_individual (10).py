2556fa3f6263484315050a07cb2af5f69257bcd7
Elena <elochka150899@gmail.com>
2017-03-30 02:15:01 +0300

    laba_3_commit

--- laba 3/laba 3_individual (10).py
+++ laba 3/laba 3_individual (10).py
@@ -10,8 +10,6 @@ while qq:
         Renault = 6
         Kia = 7
         Nissan = 8
-        Hyundai = 9
-        Mercedes_Benz = 10
 
     print("Здравствуйте, вы находитесь в программе, которая отображает сведения об автомобилях вашего города.")
     try:
@@ -30,11 +28,11 @@ while qq:
                 c = int(input("Какое количество автомобилей вы хотите зарегистрировать? Ответ: "))
                 print("Вам нужно заполнить бланк регистрации авто: ")
                 for i in range(c):
-                    marka_ = int(input("[ BMW = 1,  Volkswagen = 2,  Toyota = 3,  Ford = 4,  Skoda = 5, \n"
-                               "Renault = 6, Kia = 7,  Nissan = 8,  Hyundai = 9,  Mercedes_Benz = 10 ] \n"
+                    marka_ = int(input("[ BMW = 1,  Volkswagen = 2,  Toyota = 3,  Ford = 4,   \n"
+                               "Skoda = 5, Renault = 6, Kia = 7,  Nissan = 8 ] \n"
                                  "(если в предложенном списке нету вашей марки - вам нужна другая программа) \n"
                                  "Введите номер марки вашего авто из списка: "))
-                    if marka_ in range(10):
+                    if marka_ in range(9):
                         marka = Marka(marka_).name
                         markas.append(marka)
                     else:
@@ -62,14 +60,51 @@ while qq:
             else:
                 break
         elif a == 2:
-
+            imena = ["Александров","Шестакова","Мишина","Григорян","Шитов","Козлов","Воробьев"]
+            nom = ["1456","0954","1459","1235","9054","7895","2232"]
+            mar = ["BMW", "Volkswagen", "Toyota", "Ford", "Ford", "BMW", "Nissan"]
+            dat = ["12.12.12", "13.11.12", "24.12.11", "15.08.13", "27.03.11", "01.09.13", "22.04.12"]
             n = int(input("Ваш выбор - просмотр данных базы. Выберите сортировку для владельцев марки: \n "
                           "1- фамилии в алфавитном порядке \n"
                           "2- номера по возрастанию"))
             if n == 1:
+                m = int(input("[ BMW = 1,  Volkswagen = 2,  Toyota = 3,  Ford = 4,   \n"
+                                   "Skoda = 5, Renault = 6, Kia = 7,  Nissan = 8 ] \n"
+                                   "(если в предложенном списке нету вашей марки - вам нужна другая программа) \n"
+                                   "Введите номер марки авто из списка: "))
+                ma = Marka(m).name
+                if m in range(9):
+                    if ma in mar:
+                        for i in range(len(mar)):
+                            if mar[i] == ma:
+                                h = []
+                                h.append((mar[i], imena[i], nom[i], dat[i]))
+                                print(h)
+                                #print(imena[i])
+                                #print(nom[i])
+                                #print(dat[i])
+                    else:
+                        print("Владельцев автомобилей такой марки еще нет в базе!")
+                else:
+                    print("вы должны выбрать марку из списка")
+            elif n == 2:
+                m = int(input("[ BMW = 1,  Volkswagen = 2,  Toyota = 3,  Ford = 4,   \n"
+                              "Skoda = 5, Renault = 6, Kia = 7,  Nissan = 8 ] \n"
+                              "(если в предложенном списке нету вашей марки - вам нужна другая программа) \n"
+                              "Введите номер марки авто из списка: "))
+                ma = Marka(m).name
+                if m in range(9):
+                    if ma in mar:
+                        for i in range(len(mar)):
+                            if mar[i] == ma:
+                                h = []
+                                h.append((nom[i], imena[i], dat[i]))
+                                print(h)
+                    else:
+                        print("Владельцев автомобилей такой марки еще нет в базе!")
+                else:
+                    print("вы должны выбрать марку из списка")
 
 
-
-
-    except ValueError:
+    except (ValueError,IndexError):
         print("Введите коректные данные")

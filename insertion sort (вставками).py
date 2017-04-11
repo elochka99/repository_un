18821156b66dfce314816ec4965234988f2a9a45
Elena <elochka150899@gmail.com>
2017-04-11 20:44:17 +0300

    laba_4_commit

--- insertion sort (вставками).py
+++ insertion sort (вставками).py
@@ -5,16 +5,11 @@ for j in range(n):
     A[j] = int(input('Вводите элменты массива: '))
 print("ваш массив: ", A)
 N = len(A)
-i = 0
-flag = True
-while flag:
-    flag = False
-    for j in range(N-i-1):
-        if (A[j] > A[j+1]):
-            tmp = A[j]
-            A[j] = A[j+1]
-            A[j + 1] = tmp
-            flag = True
-    i+=1
+for i in range(1,N):
+    j = i - 1
+    key = A[i]
+    while j >= 0 and A[j] > key:
+        A[j+1] = A[j]
+        j -= 1
+    A[j+1] = key
     print(A)
-

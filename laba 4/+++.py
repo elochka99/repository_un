9b321e48d227ac88956de025e5971588f3a63080
Elena <elochka150899@gmail.com>
2017-05-01 23:12:33 +0300

    done!

--- laba 4/+++.py
+++ laba 4/+++.py
@@ -4,6 +4,7 @@ import numpy as np
 import random
 import time
 
+
 def bubble_sort(A, r):
     """
     Сортировка пузырьком
@@ -36,15 +37,16 @@ def bubble_sort(A, r):
                         flag = True
         else:
             break
-    return A,  count1,  count2
+    return A, count1, count2
+
 
 def selection_sort(A, r):
     """
     Сортировка выбором
-    
+
     Это алгоритм сортировки, в частности сортировка выбором на месте. 
     На массиве из n элементов имеет время выполнения в худшем, среднем и лучшем случае O(n**2).
-       
+
     :param A: Принимает массив.
     :param r: выбор направления сортировки
     :return: возвращает отсортированный массив, сравнения и сумму обменов.
@@ -67,15 +69,16 @@ def selection_sort(A, r):
         count2 += 1
     return A, count1, count2
 
+
 def insertion_sort(A, r):
     """
     Сортировка вставками.
-    
+
     Aлгоритм сортировки, в котором элементы входной последовательности 
     просматриваются по одному, и каждый новый поступивший элемент
     размещается в подходящее место среди ранее упорядоченных элементов.
     Вычислительная сложность — O(n**2)'''
-    
+
     :param A: Принимает массив.
     :param r: выбор направления сортировки
     :return: возвращает отсортированный массив, сравнения и сумму обменов.
@@ -98,14 +101,15 @@ def insertion_sort(A, r):
                 j -= 1
     return A, count1, count2
 
+
 def cocktail_sort(A, r):
     """
     Сортировка перемешиванием.
-    
+
     Сортировка перемешиванием, или двунаправленная — разновидность пузырьковой сортировки.
     Лучший случай для этой сортировки — отсортированный массив O(n), худший — отсортированный в обратном
     порядке O(n**2)
-    
+
     :param A: Принимает массив.
     :param r: выбор направления сортировки
     :return: возвращает отсортированный массив, сравнения и сумму обменов.
@@ -132,40 +136,42 @@ def cocktail_sort(A, r):
             if not flag:
                 return A, count1, count2
 
+
 def shell_sort(A, r):
     """
     Сортировка Шелла.
-    
+
     алгоритм сортировки, являющийся усовершенствованным вариантом сортировки вставками. 
     Идея метода Шелла состоит в сравнении элементов, стоящих не только рядом, но и на
     определённом расстоянии друг от друга.
-    
+
     :param A: Принимает массив.
     :param r: выбор направления сортировки
     :return: возвращает отсортированный массив, сравнения и сумму обменов.
     """
     count1 = 0
     count2 = 0
-    n = len(A)//2
+    n = len(A) // 2
     while n > 0:
-        n = n//2
-        for i in range (n, len(A), n):
+        n = n // 2
+        for i in range(n, len(A), n):
             j = i
             count1 += 1
             if r == 1:
-                while j>0 and A[j] < A[j-n]:
-                    A[j], A[j-n] = A[j-n], A[j]
+                while j > 0 and A[j] < A[j - n]:
+                    A[j], A[j - n] = A[j - n], A[j]
                     count2 += 1
                     j -= n
             if r == 2:
-                while j>0 and A[j] > A[j-n]:
-                    A[j], A[j-n] = A[j-n], A[j]
+                while j > 0 and A[j] > A[j - n]:
+                    A[j], A[j - n] = A[j - n], A[j]
                     count2 += 1
                     j -= n
 
-        if n ==  1:
+        if n == 1:
             break
     return A, count1, count2
+
 
 def heap_sort(li, r):
     """
@@ -174,13 +180,14 @@ def heap_sort(li, r):
     Пирамидальная сортировка — алгоритм сортировки, работающий в худшем,
     в среднем и в лучшем случае (то есть гарантированно) за О(n log n) операций при сортировке n элементов.
     Количество применяемой служебной памяти не зависит от размера массива (то есть, O(1)).
-    
+
     :param li: Принимает массив.
     :param r: выбор направления сортировки
     :return: возвращает отсортированный массив, сравнения и сумму обменов.
     """
     count1 = 0
     count2 = 0
+
     def down_Heap(li, k, n):
         """
         Функция создает бинарное сортировочное дерево
@@ -193,10 +200,10 @@ def heap_sort(li, r):
         nonlocal count2
         new_elem = li[k]
         if r == 1:
-            while 2*k+1 < n:
-                child = 2*k+1
+            while 2 * k + 1 < n:
+                child = 2 * k + 1
                 count1 += 1
-                if child+1 < n and li[child] < li[child+1]:
+                if child + 1 < n and li[child] < li[child + 1]:
                     child += 1
                     count2 += 1
                 if new_elem >= li[child]:
@@ -218,13 +225,14 @@ def heap_sort(li, r):
             li[k] = new_elem
 
     size = len(li)
-    for i in range(size//2-1,-1,-1):
+    for i in range(size // 2 - 1, -1, -1):
         down_Heap(li, i, size)
-    for i in range(size-1,0,-1):
+    for i in range(size - 1, 0, -1):
         li[i], li[0] = li[0], li[i]
         count2 += 1
         down_Heap(li, 0, i)
     return li, count1, count2
+
 
 def sort(A):
     """
@@ -317,6 +325,7 @@ def sort(A):
     print('Произведено перестановок:', res[-1])
     print("время работы: ", str(time.clock() - t))
 
+
 while True:
     vop = input(print("Вы хотите отсортировать массив [1] или посмотреть "
                       "эффективности алгоритмов на тестовм массиве [2]? - "))
@@ -344,7 +353,7 @@ while True:
                             continue
 
                         print("колчество элементов: ", n)
-                        print("Ваша последовательность чисел: ", A )
+                        print("Ваша последовательность чисел: ", A)
                         break
                     else:
                         print("Для нормальной работы программы введите число менее 500 000")
@@ -353,7 +362,8 @@ while True:
                 except ValueError:
                     print('Пожалуйста, введите целое число не более 9 символов ☻\n')
                     continue
-            print('Каким методом сортировать? \n[1 - bubble / 2 - selection / 3 - insertion / 4 - cocktail / 5 - shell / 6 - heapsort ]')
+            print(
+                'Каким методом сортировать? \n[1 - bubble / 2 - selection / 3 - insertion / 4 - cocktail / 5 - shell / 6 - heapsort ]')
             while True:
                 try:
                     ask = int(input('Выберите цифру → '))
@@ -361,12 +371,14 @@ while True:
                         order = input("1 - по возростанию \ 2 - в порядке убывания :")
                         t = time.clock()
                         if order == "1":
-                            res = str(bubble_sort(A, 1)).split( )
+                            A2 = list(A)
+                            res = str(bubble_sort(A2, 1)).split()
                             print("Отсортированная последовательность bubble_sort по возрастанию: ", res[1:-2])
                             print('Произведено сравнений:', res[-2])
                             print('Произведено перестановок:', res[-1])
                         elif order == "2":
-                            res = str(bubble_sort(A, 2)).split()
+                            A2 = list(A)
+                            res = str(bubble_sort(A2, 2)).split()
                             print("Отсортированная последовательность bubble_sort по убыванию: ", res[1:-2])
                             print('Произведено сравнений:', res[-2])
                             print('Произведено перестановок:', res[-1])
@@ -378,12 +390,14 @@ while True:
                         order = input("1 - по возростанию \ 2 - в порядке убывания :")
                         t = time.clock()
                         if order == "1":
-                            res = str(selection_sort(A, 1)).split()
+                            A2 = list(A)
+                            res = str(selection_sort(A2, 1)).split()
                             print("Отсортированная последовательность selection_sort по возрастанию: ", res[1:-2])
                             print('Произведено сравнений:', res[-2])
                             print('Произведено перестановок:', res[-1])
                         elif order == "2":
-                            res = str(selection_sort(A, 2)).split()
+                            A2 = list(A)
+                            res = str(selection_sort(A2, 2)).split()
                             print("Отсортированная последовательность selection_sort по убыванию: ", res[1:-2])
                             print('Произведено сравнений:', res[-2])
                             print('Произведено перестановок:', res[-1])
@@ -395,12 +409,14 @@ while True:
                         t = time.clock()
                         order = input("1 - по возростанию \ 2 - в порядке убывания :")
                         if order == "1":
-                            res = str(insertion_sort(A, 1)).split()
+                            A2 = list(A)
+                            res = str(insertion_sort(A2, 1)).split()
                             print("Отсортированная последовательность insertion_sort по возрастанию: ", res[1:-2])
                             print('Произведено сравнений:', res[-2])
                             print('Произведено перестановок:', res[-1])
                         elif order == "2":
-                            res = str(insertion_sort(A, 2)).split()
+                            A2 = list(A)
+                            res = str(insertion_sort(A2, 2)).split()
                             print("Отсортированная последовательность insertion_sort по убыванию: ", res[1:-2])
                             print('Произведено сравнений:', res[-2])
                             print('Произведено перестановок:', res[-1])
@@ -412,12 +428,14 @@ while True:
                         t = time.clock()
                         order = input("1 - по возростанию \ 2 - в порядке убывания :")
                         if order == "1":
-                            res = str(cocktail_sort(A, 1)).split()
+                            A2 = list(A)
+                            res = str(cocktail_sort(A2, 1)).split()
                             print("Отсортированная последовательность cocktail_sort по возрастанию: ", res[1:-2])
                             print('Произведено сравнений:', res[-2])
                             print('Произведено перестановок:', res[-1])
                         elif order == "2":
-                            res = str(cocktail_sort(A, 2)).split()
+                            A2 = list(A)
+                            res = str(cocktail_sort(A2, 2)).split()
                             print("Отсортированная последовательность cocktail_sort по убыванию: ", res[1:-2])
                             print('Произведено сравнений:', res[-2])
                             print('Произведено перестановок:', res[-1])
@@ -429,12 +447,14 @@ while True:
                         t = time.clock()
                         order = input("1 - по возростанию \ 2 - в порядке убывания :")
                         if order == "1":
-                            res = str(shell_sort(A, 1)).split()
+                            A2 = list(A)
+                            res = str(shell_sort(A2, 1)).split()
                             print("Отсортированная последовательность shell_sort по возрастанию: ", res[1:-2])
                             print('Произведено сравнений:', res[-2])
                             print('Произведено перестановок:', res[-1])
                         elif order == "2":
-                            res = str(shell_sort(A, 2)).split()
+                            A2 = list(A)
+                            res = str(shell_sort(A2, 2)).split()
                             print("Отсортированная последовательность shell_sort по убыванию: ", res[1:-2])
                             print('Произведено сравнений:', res[-2])
                             print('Произведено перестановок:', res[-1])
@@ -446,19 +466,21 @@ while True:
                         t = time.clock()
                         order = input("1 - по возростанию \ 2 - в порядке убывания :")
                         if order == "1":
-                            res = str(heap_sort(A, 1)).split()
+                            A2 = list(A)
+                            res = str(heap_sort(A2, 1)).split()
                             print("Отсортированная последовательность heap_sort по возрастанию: ", res[1:-2])
                             print('Произведено сравнений:', res[-2])
                             print('Произведено перестановок:', res[-1])
                         elif order == "2":
-                            res = str(heap_sort(A, 2)).split()
+                            A2 = list(A)
+                            res = str(heap_sort(A2, 2)).split()
                             print("Отсортированная последовательность heap_sort по убыванию: ", res[1:-2])
                             print('Произведено сравнений:', res[-2])
                             print('Произведено перестановок:', res[-1])
                         else:
                             print('Пожалуйста, введите корректные данные ☻\n')
                             continue
-                        print("время работы: ", str( time.clock() - t))
+                        print("время работы: ", str(time.clock() - t))
                     else:
                         print('Пожалуйста, введите корректные данные ☻\n')
                         continue
@@ -476,10 +498,11 @@ while True:
             A = np.zeros(n, dtype=int)
             for j in range(n):
                 A[j] = random.randint(0, 100000)
-            qwer = input(print("Вывести\n [1] - эффективность для сгенерированной псевдослучайной последовательности \n "
-                               "[2] - эффективность для отсортированной последовательности в порядке неубывания \n"
-                               "[3] - эффективность для отсортированной последовательности в порядке невозрастания\n"
-                               "Ваш ответ - "))
+            qwer = input(
+                print("Вывести\n [1] - эффективность для сгенерированной псевдослучайной последовательности \n "
+                      "[2] - эффективность для отсортированной последовательности в порядке неубывания \n"
+                      "[3] - эффективность для отсортированной последовательности в порядке невозрастания\n"
+                      "Ваш ответ - "))
 
             if qwer == "1":
                 print("количество элементов в массиве: ", n)
@@ -491,7 +514,7 @@ while True:
                 print("отсортированный массив в порядке неубывания: \n", B)
                 print(sort(B))
             elif qwer == "3":
-                C = sorted(A, reverse = True)
+                C = sorted(A, reverse=True)
                 print("количество элементов в массиве: ", n)
                 print("отсортированный массив в порядке невозрастания: \n", C)
                 print(sort(C))

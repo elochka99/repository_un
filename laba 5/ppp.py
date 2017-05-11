e45d24f1d80e9df2c1b5c45463232f06e60ebad8
Elena <elochka150899@gmail.com>
2017-05-11 21:50:15 +0300

    laba 5

--- laba 5/ppp.py
+++ laba 5/ppp.py
@@ -1,7 +1,7 @@
 # Горохова Елена КНИТ 16-А
 # Сформировать функцию подсчета количества разных представлений заданного натурального числа в виде
 # суммы не менее двух попарно различных натуральных слагаемых
-
+import numpy as np
 def rec_decompose(c):
     def func(c, id):
         if c == 0 and id == 0:
@@ -15,9 +15,52 @@ def rec_decompose(c):
     for i in range(1, c):
         stack.append(func(c, i))
     return max(stack)
-def func_iter(c):
-    a = c - 1
-    return len(list(enumerate(range(a, c // 2 - (c - a), -1))))
+def number_decompose_iter(n):
+    """ Функция подсчета количества различных представлений заданого числа.
+     Итерационно
+
+    :param n: заданное натуральное число 
+    :return: количество различных представлений числа n 
+    """
+    a = 0
+    n1 = n
+    c = 0  # количество представлений числа n
+    while n1 >= 0:
+        a += 1
+        n1 -= a
+    mas = np.zeros(a - 1, dtype=int)
+    for i in range(0, a - 1):
+        mas[i] = i + 1
+    if n1 != 0:
+        mas[a - 2] = mas[a - 3] + a + 1 + n1
+    a -= 1
+    for i in range(0, a):
+        pass
+    c += 1
+    while a > 1:
+        for j in range(a - 2, -1, -1):
+            temp1 = mas[j]
+            temp2 = mas[a - 1]
+            while mas[j] + 1 < mas[a - 1] - 1:
+                mas[j] += 1
+                mas[a - 1] -= 1
+                n1 = 0
+                for i in range(0, a - 1):
+                    for i1 in range(i + 1, a):
+                        if mas[i] == mas[i1]:
+                            n1 = 1
+                if n1 == 0:
+                    for i in range(0, a):
+                        pass
+                    c += 1
+            mas[j] = temp1
+            mas[a - 1] = temp2
+        mas[a - 2] += mas[a - 1]
+        a -= 1
+    if n > 5:
+        return c + 1
+    else:
+        return c
 
 def rec(n, m):
     '''
@@ -64,8 +107,9 @@ while True:
         else:
             kol = rec(n,m)
             kol1 = iter(n, m)
-            print('Количество представлений заданного числа: ',"рекурсивно - ",kol, "итерационно - ", kol1)
-            print("количество представлений без 1+1+1... = ", rec_decompose(n) )
+            print('Количество представлений заданного числа: ',"\nрекурсивно - ",kol, "\nитерационно - ", kol1)
+            print("без 1+1+1... рекурсивно = ", rec_decompose(n) )
+            print("без 1+1+1... итерационно = ", number_decompose_iter(n))
         w = input('\nХотите начать работу с программой заново? [1 - да]: ')
         if w == '1':
             print()
